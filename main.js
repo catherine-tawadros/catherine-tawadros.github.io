a533a02f10fd3571ac8f9e767ae3bf3a79f8fca7
catherine-tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2020-11-19 03:25:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ function Person(u, p, t)
 
 function loginAttempt()
 {
+      function Person(u, p, t)
+      {
+            this.username=u;
+            this.password=p;
+           this.type=t;
+      }
       var people = [new Person("1","1","student"), new Person("2","2","teacher")];
       var username = document.getElementById("username").value;
       var password =  document.getElementById("password").value;
