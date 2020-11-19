48c27aab1e54864403d7882efcda93476eb5f0c3
catherine-tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2020-11-19 03:34:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ function loginAttempt()
             this.password = p;
             this.what = t;
       }
-      var people = [new Person(1,1,"student"), new Person(2,2,"teacher")];
+      var people = [new Person("1","1","student"), new Person("2","2","teacher")];
       var username = document.getElementById("username").value;
       var password =  document.getElementById("password").value;
       for (var i=0; i<people.length; i++)
       {
-            if (username==people[i].username && password==people[i].password)
+            if (username == people[i].username && password == people[i].password)
             {
                   if (people[i].what == "student") 
                         {location.assign("studenthome.html");}
@@ -27,5 +27,6 @@ function loginAttempt()
              }
             else
                   {location.assign("error.html");}
+      }
                                          
 }                                                                 
