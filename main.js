bbd38398de0225842e9d512fef8bfbd304986876
catherine-tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2020-11-19 02:32:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function loginAttempt()
 {
       var username = document.getElementById("username").value;
       var password =  document.getElementById("password").value;
-      if (username==1 && password==1)
+      if (username==Person.username && password==Person.password)
             {location.assign('home.html');}
       else
             {location.assign('error.html');}
