33940422102cdf7678468d78e16f4b28962ed779
Catherine Tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2020-11-21 15:44:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var people = [];
-window.alert("is this a thing?");
+//window.alert("is this a thing?");
 
 function Person(a,b,c,d)
 {
@@ -13,30 +13,30 @@ function Person(a,b,c,d)
 
 function createAccount()
 {
-      window.alert("checkpoint a");
+      //window.alert("checkpoint a");
       user = new Person(document.getElementById("newFirstName").value,document.getElementById("newLastName").value,
              document.getElementById("newUsername").value,document.getElementById("newPassword").value);
       people.push(user);
-      window.alert("checkpoint b");
+      //window.alert("checkpoint b");
       location.assign("index.html");
 }
 
 
 function loginAttempt()
 {
-      window.alert("checkpoint 1");
+      //window.alert("checkpoint 1");
       var people = [];
-      window.alert("checkpoint 2");
+      //window.alert("checkpoint 2");
       people.push(new Person("A","m","s","s"), new Person("f","d","t","t"));
-      window.alert("checkpoint 3");
+      //window.alert("checkpoint 3");
       var username = document.getElementById("username").value;
       var password =  document.getElementById("password").value;
-      window.alert("checkpoint 4");
+      //window.alert("checkpoint 4");
       for (var i=0; i<people.length; i++)
       {
             if (username == people[i].username && password == people[i].password)
             {
-                  window.alert("checkpoint 5");
+                  //window.alert("checkpoint 5");
                   if (people[i].what == "student") 
                         {location.assign("studenthome.html");}
                   if (people[i].what == "teacher") 
