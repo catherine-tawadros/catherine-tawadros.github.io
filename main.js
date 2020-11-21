daa0522324166262cde6f0f4248c8dbaacd91a8b
Catherine Tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2020-11-20 23:00:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -29,9 +29,9 @@ people += new Person("f","d","t","t");
 
 function loginAttempt()
 {
-      window.alert();
       var username = document.getElementById("username").value;
       var password =  document.getElementById("password").value;
+      window.alert();
       for (var i=0; i<people.length; i++)
       {
             if (username == people[i].username && password == people[i].password)
