1a5d632061b21b34f56ca79c69a5dbff512704b7
Catherine Tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2020-11-21 15:49:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var people = [];
+let people = [];
 //window.alert("is this a thing?");
 
 function Person(a,b,c,d)
@@ -25,14 +25,13 @@ function createAccount()
 function loginAttempt()
 {
       //window.alert("checkpoint 1");
-      var people = [];
       //window.alert("checkpoint 2");
       people.push(new Person("A","m","s","s"), new Person("f","d","t","t"));
       //window.alert("checkpoint 3");
-      var username = document.getElementById("username").value;
-      var password =  document.getElementById("password").value;
+      let username = document.getElementById("username").value;
+      let password =  document.getElementById("password").value;
       //window.alert("checkpoint 4");
-      for (var i=0; i<people.length; i++)
+      for (let i=0; i<people.length; i++)
       {
             if (username == people[i].username && password == people[i].password)
             {
