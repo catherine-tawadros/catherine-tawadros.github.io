a9da8cb7884a6942e49ac21095ab8637fc8707d8
Catherine Tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2020-11-25 01:22:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let people = [];
-//window.alert("is this a thing?");
 
 class Person
 {
@@ -17,42 +16,38 @@ class Person
 
 function createAccount()
 {
-    window.alert("checkpoint a");
-    let people = [];
+    window.alert("in the function");
+    var people;
     people.push(new Person("a","a","a","a"));
     for (let prsn in people)
     {
+        window.alert("in the for loop");
         if (prsn.username == document.getElementById("username").value)
         { window.alert("this username is taken"); return; }
         if (prsn.username == document.getElementById("password").value)
         { window.alert("this password is taken"); return; }
     }
-    window.alert("check");
+    window.alert("after for loop");
     if (document.getElementById("password").value.length < 5)
         {window.alert("please enter a password that is at least 5 characters long"); return; }
-    window.alert("checkpoint b");
+    window.alert("almost there");
     user = new Person(document.getElementById("newFirstName").value, document.getElementById("newLastName").value,
              document.getElementById("newUsername").value, document.getElementById("newPassword").value);
     people.push(user);
-    window.alert("checkpoint c");
+    window.alert("end");
     location.assign("index.html");
 }
 
 
 function loginAttempt()
 {
-      //window.alert("checkpoint 1");
-      //window.alert("checkpoint 2");
       people.push(new Person("A","m","s","s"), new Person("f","d","t","t"));
-      //window.alert("checkpoint 3");
       let username = document.getElementById("username").value;
       let password =  document.getElementById("password").value;
-      //window.alert("checkpoint 4");
       for (let i=0; i<people.length; i++)
       {
             if (username == people[i].username && password == people[i].password)
             {
-                  //window.alert("checkpoint 5");
                   if (people[i].what == "student") 
                         {location.assign("studenthome.html");}
                   if (people[i].what == "teacher") 
