f3a0af8078e50f02f1e7bd0738b9ae9fcf35931e
Catherine Tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2021-02-08 23:40:58 -0500

    Create oob.js

---
+++ oob.js
@@ -0,0 +1,19 @@
+function oob()
+{
+  word = document.getElementById("word");
+  let new_word = "";
+  for (let i=0; i<word.length(); i++)
+    if (isVowel(word.substring(i,i+1)))
+      new_word+="oob";
+    else new_word+=word.substring(i,i+1);
+  return new_word;
+}
+
+function isVowel(a)
+{
+   let vowls="aeiou";
+   for (let i=0; i<5; i++)
+    if (vowels.substring(i,i+1).toLowerCase().equals(a))
+      return true;
+   return false;
+}
