89a4cf6e9c4df799d5f89fff691456128a55f095
Catherine Tawadros <67174733+catherine-tawadros@users.noreply.github.com>
2021-03-02 17:07:09 -0500

    Update files.java

--- files.java
+++ files.java
@@ -1,6 +1,31 @@
 import java.io;
 import java.util;
 
+public class Time{
+  private int min;
+  private int second;
+  public Time(m, s) {minute=m; second=s;}
+  public Time getTime() {return m+":"+s;}
+}
+
+public class Comment{
+ private Time starttime;
+ private Time endtime;
+ private String commenter;
+ private String feedback;
+ private String owner;
+ private String videotitle;
+ public Comment(Time st, Time et, String c, String o, String fb, String vt)
+ {
+   starttime = st;
+   endtime = et;
+   commenter = c;
+   owner = o;
+   feedback = fb;
+   videotitle = vt;
+ }
+}
+
 public class FileThings{
   public static String[] getThings()
   {
